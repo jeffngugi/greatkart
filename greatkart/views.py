ea897d3c2ed51a50cddf38539dbd83d699a2d2b4
jeffngugi <ngugimwathi254@gmail.com>
2025-09-07 03:04:32 +0300

    Store product with count

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from store.models import Product
+
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.filter(is_available=True).order_by("created_date")
+    context = {"products": products}
+    return render(request, "home.html", context)
