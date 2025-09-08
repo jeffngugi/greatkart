0f1023587d2a8fce97f882b84012996f57a067b3
jeffngugi <ngugimwathi254@gmail.com>
2025-09-08 22:58:46 +0300

    feat: view single product

--- store/views.py
+++ store/views.py
@@ -16,8 +16,19 @@ def store(request, category_slug=None):
     else:
         products = Product.objects.filter(is_available=True)
         product_count = products.count()
+    print(products)
     context = {
         'products': products,
         'product_count': product_count,
     }
     return render(request, "store/store.html", context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, "store/product_detail.html", context)
