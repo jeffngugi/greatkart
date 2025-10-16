2e0d45750d98b3fad54b73f31e934603f33977e1
jeffngugi <ngugimwathi254@gmail.com>
2025-10-17 02:13:08 +0300

    user registration

--- greatkart/views.py
+++ greatkart/views.py
@@ -8,3 +8,4 @@ def home(request):
     products = Product.objects.filter(is_available=True).order_by("created_date")
     context = {"products": products}
     return render(request, "home.html", context)
+
